cdf48dc5ca03d4e610a9b6b304da4571ffbffb93
837132230@qq.com <837132230@qq.com>
2018-12-09 14:25:42 +0800

    修改首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: '生姜的博客',
-    description: '我的博客',
+    description: '个人博客',
     head: [    // 注入到当前页面的 HTML <head> 中的标签
         // ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
@@ -10,14 +10,11 @@ module.exports = {
     },
     themeConfig: {
         nav:[ // 导航栏配置
-            {text: '前端基础', link: '/accumulate/' },
-            {
-                text: '语言',
-                items: [
-                    { text: 'Chinese', link: '/language/chinese' },
-                    { text: 'Japanese', link: '/language/japanese' }
-                ]
-            }
-        ]
+            {text: '前端', link: '/frontEndBasic/' },
+            {text: 'Node', link: '/node/' },
+            {text: 'GitHub', link: 'https://github.com/HuangShengJiang' },
+        ],
+        search: false,
+        searchMaxSuggestions: 10
     }
 }
---
+++ docs/README.md
@@ -0,0 +1,12 @@
+---
+home: true
+heroImage: /img/portrait.jpg
+features:
+- title: 个人简历
+  details: 自我介绍,只为认识更多牛逼的人
+- title: 学习心得
+  details: 用于总结和记录在前端学习中遇到的问题和解决思路
+- title: 有趣的库
+  details: 收藏并学习一些好用的库
+footer: MIT Licensed | Copyright © 2018-present Evan You
+---
