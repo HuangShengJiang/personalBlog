24204a3b617636243d91c4c42377b6a0bd8aaf89
837132230@qq.com <837132230@qq.com>
2018-12-15 12:47:09 +0800

    1、修改博客目录 2、完善个人简历

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,24 @@
 module.exports = {
-    title: '生姜的博客',
-    description: '个人博客',
+    title: 'Blog',
+    description: 'Blog',
     head: [    // 注入到当前页面的 HTML <head> 中的标签
         // ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
-        lineNumbers: false // 代码块显示行号
+        lineNumbers: false, // 代码块显示行号
     },
     themeConfig: {
         nav:[ // 导航栏配置
-            {text: '前端', link: '/frontEndBasic/' },
+            { text: "Home", link: "/" },
+            {text: 'JS', link: '/frontEndBasic/' },
             {text: 'Node', link: '/node/' },
-            {text: 'GitHub', link: 'https://github.com/HuangShengJiang' },
+            {text: 'About', link: '/about/' },
+            {text: 'GitHub', link: 'https://github.com/HuangShengJiang' }
         ],
+        sidebar:  {
+
+        },
         search: false,
         searchMaxSuggestions: 10
     }
--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,7 @@ home: true
 heroImage: /img/portrait.jpg
 features:
 - title: 个人简历
-  details: 自我介绍,只为认识更多牛逼的人
+  details: 自我介绍
 - title: 学习心得
   details: 用于总结和记录在前端学习中遇到的问题和解决思路
 - title: 有趣的库
